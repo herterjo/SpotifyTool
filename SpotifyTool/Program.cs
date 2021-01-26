e5b524347cee984f34754ce8a30b073aef4c8004
Johannes <herterjo@web.de>
2021-01-26 20:58:31 +0100

    Add project files.

---
+++ SpotifyTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpotifyTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
